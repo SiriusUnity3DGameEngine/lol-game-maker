db2b0c2bb02451bcc8cc3f4945e7d6aa95df1862
liteng <930372551@qq.com>
2018-01-17 21:33:55 +0800

    add UiDroppable

--- src/ui/interaction/UiDroppable.js
+++ src/ui/interaction/UiDroppable.js
@@ -8,17 +8,25 @@ import { UiControl } from '../UiControl';
 function UiDroppable(options) {
     UiInteraction.call(this, options);
     options = options || {};
+    this.dispatch = d3.dispatch('drop');
 }
 
 UiDroppable.prototype = Object.create(UiInteraction.prototype);
 UiDroppable.prototype.constructor = UiDroppable;
 
 UiDroppable.prototype.apply = function(control) {
-    if (control instanceof UiControl) {
-        $(control.el).droppable();
-    } else {
-        $(control).droppable();
-    }
+    var _this = this;
+    var el = control instanceof UiControl ? control.el : control;
+    $(el).droppable();
+    $(el).droppable({
+        drop: function(event, ui) {
+            _this.dispatch.call('drop', _this, event, ui);
+        }
+    });
+};
+
+UiDroppable.prototype.on = function(eventName, callback) {
+    this.dispatch.on(eventName, callback);
 };
 
 export { UiDroppable };

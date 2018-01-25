383a6caa6b77c1630400218cb0f86fc8f30382fa
liteng <930372551@qq.com>
2018-01-25 22:15:30 +0800

    add stats.js, and remove useless jquery-ui css

--- src/scene/WebGLScene.js
+++ src/scene/WebGLScene.js
@@ -16,6 +16,8 @@ function WebGLScene(options) {
         1000);
     this.renderer = options.renderer || new THREE.WebGLRenderer();
     this.renderer.setSize(this.width, this.height);
+    this.stats = new Stats();
+    this.stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
     this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
     this.controls.maxPolarAngle = Math.PI * 0.5;
     this.gridHelper = new THREE.GridHelper(500, 50);
@@ -26,6 +28,8 @@ WebGLScene.prototype.constructor = WebGLScene;
 
 WebGLScene.prototype.render = function() {
     this.parent.appendChild(this.renderer.domElement);
+    this.stats.dom.style.position = 'absolute';
+    this.parent.appendChild(this.stats.dom);
     var _this = this;
     this.app.mainPanel.on('activate.WebGLScene', function(event, ui) {
         _this.runProgram.call(_this);
@@ -40,20 +44,30 @@ WebGLScene.prototype.runProgram = function() {
 };
 
 WebGLScene.prototype.createScene = function() {
+    // You can add anything to the scene here.
     var geometry = new THREE.BoxGeometry(5, 5, 5);
     var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    this.scene.add(cube);
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
     this.camera.position.set(10, 10, 10);
     this.camera.lookAt(0, 0, 0);
 };
 
 WebGLScene.prototype.renderProc = function() {
+    this.stats.begin();
+    this.animate();
     this.renderer.render(this.scene, this.camera);
+    this.stats.end();
     var _this = this;
     requestAnimationFrame(function() {
         _this.renderProc.call(_this);
     });
 }
+
+WebGLScene.prototype.animate = function() {
+    // You can do some animation here.
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+};
 
 export { WebGLScene };
